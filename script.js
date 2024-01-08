ce75caba4b977dc43750eb78748d4f913c0ac370
Mohammad Ullah <Ullahmohammad314@gmail.com>
2024-01-09 01:58:40 +0600

    Refactor hero section, added mobile responsive codes and update social media links

---
+++ script.js
@@ -0,0 +1,62 @@
+ // Function to check if an element is in the viewport
+ function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  // Function to animate the count
+  function animateCount(countTextElement) {
+    var endValue = parseInt(countTextElement.dataset.endValue);
+    var duration = 1.5; // Change the duration as needed
+
+    var startValue = 0;
+    var increment = Math.ceil(endValue / (duration * 60));
+
+    var timer = setInterval(function () {
+      startValue += increment;
+      countTextElement.textContent = startValue.toLocaleString();
+
+      if (startValue >= endValue) {
+        clearInterval(timer);
+        countTextElement.textContent = endValue.toLocaleString() + "+";
+      }
+    }, 16);
+
+    // Add the "visible" class to make the count text visible during the animation
+    countTextElement.classList.add("visible");
+  }
+
+  // Function to handle the scroll event
+  function handleScroll() {
+    // Find the count text elements
+    var countTextElements = document.querySelectorAll(".count-text");
+
+    // Check if the count text elements are in the viewport
+    countTextElements.forEach(function (countText) {
+      if (
+        isInViewport(countText) &&
+        !countText.classList.contains("visible")
+      ) {
+        animateCount(countText);
+      }
+    });
+
+    // Remove the scroll event listener after animating the counts
+    if (
+      countTextElements.length > 0 &&
+      document.querySelectorAll(".count-text.visible").length ===
+        countTextElements.length
+    ) {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }
+
+  // Add the scroll event listener
+  window.addEventListener("scroll", handleScroll);
